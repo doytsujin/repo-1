8e3aec102c9612c296070c0048115cc67ed648e1
Sainnhe Park <sainnhe@gmail.com>
2021-11-19 14:54:29 +0800

    x86_64(apps-extra-meta): update hook

--- archlinux/src/apps-extra-meta/patch.sh
+++ archlinux/src/apps-extra-meta/patch.sh
@@ -3,7 +3,9 @@
 if [ "${1}" = "-R" ]; then
   sed -i 's/^Icon=.*/Icon=firefox-developer-edition/' /usr/share/applications/firefox-developer-edition.desktop
   sed -i 's/^Icon=.*/Icon=Alacritty/' /usr/share/applications/Alacritty.desktop
+  sed -i 's/^Exec=.*/Exec=\/opt\/apps\/com.qq.office.deepin\/files\/run.sh' /usr/share/applications/com.qq.office.deepin.desktop
 else
   sed -i 's/^Icon=.*/Icon=firefox/' /usr/share/applications/firefox-developer-edition.desktop
   sed -i 's/^Icon=.*/Icon=Terminal/' /usr/share/applications/Alacritty.desktop
+  sed -i 's/^Exec=.*/Exec=bwrap --dev-bind \/ \/ --tmpfs \/usr\/share\/fonts\/TTF\/ \/opt\/apps\/com.qq.office.deepin\/files\/run.sh/' /usr/share/applications/com.qq.office.deepin.desktop
 fi
