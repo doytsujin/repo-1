c8609399a43b2e0833adcca244130ec565844bb5
Sainnhe Park <i@sainnhe.dev>
2022-08-21 11:30:17 +0800

    Fix sed command in apps-extra-meta

--- archlinux/src/apps-extra-meta/patch.sh
+++ archlinux/src/apps-extra-meta/patch.sh
@@ -2,7 +2,7 @@
 
 if [ "${1}" = "-R" ]; then
   sed -i 's/^Icon=.*/Icon=firefox-developer-edition/' /usr/share/applications/firefox-developer-edition.desktop
-  sed -i 's/^Exec=.*/Exec=\/opt\/apps\/com.qq.office.deepin\/files\/run.sh' /usr/share/applications/com.qq.office.deepin.desktop
+  sed -i 's/^Exec=.*/Exec=\/opt\/apps\/com.qq.office.deepin\/files\/run.sh/' /usr/share/applications/com.qq.office.deepin.desktop
 else
   sed -i 's/^Icon=.*/Icon=firefox/' /usr/share/applications/firefox-developer-edition.desktop
   sed -i 's/^Exec=.*/Exec=bwrap --dev-bind \/ \/ --tmpfs \/usr\/share\/fonts\/TTF\/ \/opt\/apps\/com.qq.office.deepin\/files\/run.sh/' /usr/share/applications/com.qq.office.deepin.desktop
